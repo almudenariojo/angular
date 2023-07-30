92a156ab511b11890b408ba4bfa8ad3f06e5f756
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-07-31 00:24:08 +0200

    subir

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -7,18 +7,34 @@ import { Zapatilla } from '../models/zapatilla';
 })
 export class ZapatillasComponent implements OnInit {
   public titulo: string;
-  public zapatillas: Array<Zapatilla>; // Aquí se especifica el tipo de datos
+  public zapatillas: Array<Zapatilla>;
+  public marcas: string[];
 
   constructor() {
     this.titulo = "Componente de zapatillas";
     this.zapatillas = [
-      new Zapatilla ('Reebook Classic', 'Reebook', 'red', 30, true),
-      new Zapatilla ('Nike Classic', 'Nike', 'negro', 80, true),
-      new Zapatilla ('Adidas Classic', 'Adidas', 'rojo', 120, false)
-    ]; // Inicializar el array vacío si no hay datos disponibles
+      new Zapatilla('Reebook Classic', 'Reebook', 'red', 30, true),
+      new Zapatilla('Nike Classic', 'Nike', 'negro', 80, true),
+      new Zapatilla('Adidas Classic', 'Adidas', 'rojo', 120, false),
+      new Zapatilla('Adidas Moder', 'Adidas', 'azul', 120, true)
+    ];
+    this.marcas = [];
     console.log("Se ha cargado el componente zapatillas.component.ts");
   }
-  ngOnInit(){
-      console.log(this.zapatillas);
+
+  ngOnInit() {
+    console.log(this.zapatillas);
+    this.obtenerMarcas();
   }
+
+  obtenerMarcas() {
+    this.zapatillas.forEach((zapatilla, index) => {
+      if (!this.marcas.includes(zapatilla.marca)) {
+        this.marcas.push(zapatilla.marca);
+      }
+    });
+    console.log(this.marcas);
+  }
+
 }
+
